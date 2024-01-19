7374e6f336e4b8cce299682ab0008e078c34afb3
dawwson <shines1427@gmail.com>
2024-01-19 15:53:21 +0900

    Fix: 브라우저 기본 동작 수행 방지

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("#login-form input");
 const loginButton = loginForm.querySelector("#login-form button");
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+}
+
+function handleLinkClick(event) {
+  event.preventDefault();
+}
+// 엔터나 버튼을 클릭했을 때 submit 이벤트 발생
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
