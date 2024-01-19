0f40900ad8838d7c93ebcf8d787b3b3cd5f0c7ff
dawwson <shines1427@gmail.com>
2024-01-19 16:27:19 +0900

    Feat: 로컬스토리지에 username 저장

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   // 기본동작(페이지 새로고침) 방지
@@ -12,10 +13,24 @@ function onLoginSubmit(event) {
   const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASSNAME);
 
+  // 로컬 스토리지에 username 저장
+  localStorage.setItem(USERNAME_KEY, username);
+
   // 인삿말 보이게 하기
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-// 엔터나 버튼을 클릭했을 때 submit 이벤트 발생
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (!savedUsername) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  // 엔터나 버튼을 클릭했을 때 submit 이벤트 발생
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
