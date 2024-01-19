3e05e44fb534a7a8f851d20d5fd217c3353afc7a
dawwson <shines1427@gmail.com>
2024-01-19 16:10:19 +0900

    Feat: submit 할 때 form 숨기고, h1 요소 보이게 함

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("#login-form input");
-const loginButton = loginForm.querySelector("#login-form button");
-const link = document.querySelector("a");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
+  // 기본동작(페이지 새로고침) 방지
   event.preventDefault();
+
+  // form 숨기기
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+
+  // 인삿말 보이게 하기
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event) {
-  event.preventDefault();
-}
 // 엔터나 버튼을 클릭했을 때 submit 이벤트 발생
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
