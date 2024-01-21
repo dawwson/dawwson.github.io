51199c725e701e438f8b7f9fd323c222bc498e8f
dawwson <shines1427@gmail.com>
2024-01-21 19:40:29 +0900

    Feat: 투두 리스트 추가

---
+++ js/todo.js
@@ -0,0 +1,27 @@
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.querySelector("#todo-list");
+
+function handleToDoSubmit(event) {
+  // 브라우저 기본동작(페이지 새로고침) 방지
+  event.preventDefault();
+
+  // 입력값 가져와서 리스트에 추가
+  const newToDo = toDoInput.value;
+  paintToDo(newToDo);
+
+  // input 비우기
+  toDoInput.value = "";
+}
+
+function paintToDo(toDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+
+  span.innerText = toDo;
+  li.appendChild(span);
+
+  toDoList.appendChild(li);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
