b5a573cde094446061315905beef90f17e804206
dawwson <shines1427@gmail.com>
2024-01-22 00:55:19 +0900

    Refactor: then -> async/await으로 수정

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,20 @@
 // TODO: API Key 관리 방법 고민해보기
 const API_KEY = "";
 
-function onGeoOk(position) {
+async function onGeoOk(position) {
   const lat = position.coords.latitude; // 위도
   const lon = position.coords.longitude; // 경도
 
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
 
-  fetch(url).then((res) => {
-    res.json().then((data) => {
-      const weather = document.querySelector("#weather span:first-child");
-      const city = document.querySelector("#weather span:last-child");
+  const response = await fetch(url);
+  const data = await response.json();
 
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-      city.innerText = data.name;
-    });
-  });
+  const weather = document.querySelector("#weather span:first-child");
+  const city = document.querySelector("#weather span:last-child");
+
+  weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+  city.innerText = data.name;
 }
 
 function onGeoError() {
