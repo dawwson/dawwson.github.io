e1b558b0418118ad43513f0f68c3a49b02694f6c
dawwson <shines1427@gmail.com>
2024-01-21 20:33:01 +0900

    Feat: 로컬 스토리지에 투두 리스트 저장

--- js/todo.js
+++ js/todo.js
@@ -2,17 +2,8 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
-function handleToDoSubmit(event) {
-  // 브라우저 기본동작(페이지 새로고침) 방지
-  event.preventDefault();
-
-  // 입력값 가져와서 리스트에 추가
-  const newToDo = toDoInput.value;
-  paintToDo(newToDo);
-
-  // input 비우기
-  toDoInput.value = "";
-}
+const TODOS_KEY = "todos";
+let toDos = [];
 
 function handleDeleteToDo(event) {
   // 버튼의 부모 요소인 li를 삭제
@@ -35,4 +26,34 @@ function paintToDo(toDo) {
   toDoList.appendChild(li);
 }
 
+function saveToDos(toDos) {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+function handleToDoSubmit(event) {
+  // 브라우저 기본동작(페이지 새로고침) 방지
+  event.preventDefault();
+
+  // 입력값 가져와서 로컬 스토리지에 저장
+  const newToDo = toDoInput.value;
+  toDos.push(newToDo);
+  saveToDos(toDos);
+
+  // 리스트에 추가하기
+  paintToDo(newToDo);
+
+  // input 비우기
+  toDoInput.value = "";
+}
+
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+// 로컬 스토리지에 저장된 리스트 조회
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos) {
+  const parsedToDos = JSON.parse(savedToDos);
+  // 저장되어 있던 리스트로 복원
+  toDos = parsedToDos;
+  parsedToDos.forEach((toDo) => paintToDo(toDo));
+}
